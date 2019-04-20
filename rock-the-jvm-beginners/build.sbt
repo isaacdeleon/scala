bae274e26dc77d8302754f2bd7e391e98a5f370f
isaacdeleon <marcodeleonmx@gmail.com>
2019-04-19 20:47:47 -0500

    Revert "first coomit scala"
    This reverts commit 98ac18ff1f639dfe0b12a82f4ba5c056055d7991.

--- rock-the-jvm-beginners/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "rock-the-jvm-beginners"
-
-version := "0.1"
-
-scalaVersion := "2.12.8"
--- rock-the-jvm-beginners/src/main/scala/playground/JavaPlayground.java
+++
@@ -1,8 +0,0 @@
-package playground;
-
-public class JavaPlayground {
-
-    public static void main(String[]args) {
-        System.out.println("Hello Java");
-    }
-}
